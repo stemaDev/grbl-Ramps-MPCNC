971d582e8fc84dac377070fa6c55fd4a496fd837
stemaDev <reg@stemadev.com>
2021-10-29 10:45:16 +0200

    steppers and limit switches with pinout helpers (still default layout)

---
+++ src/mega_pinout.h
@@ -0,0 +1,209 @@
+#define D0_PORT E
+#define D0_BIT 0
+
+#define D1_PORT E
+#define D1_BIT 1
+
+#define D2_PORT E
+#define D2_BIT 4
+
+#define D3_PORT E
+#define D3_BIT 5
+
+#define D4_PORT G
+#define D4_BIT 5
+
+#define D5_PORT E
+#define D5_BIT 3
+
+#define D6_PORT H
+#define D6_BIT 3
+
+#define D7_PORT H
+#define D7_BIT 4
+
+#define D8_PORT H
+#define D8_BIT 5
+
+#define D9_PORT H
+#define D9_BIT 6
+
+#define D10_PORT B
+#define D10_BIT 4
+
+#define D11_PORT B
+#define D11_BIT 5
+
+#define D12_PORT B
+#define D12_BIT 6
+
+#define D13_PORT B
+#define D13_BIT 7
+
+#define D14_PORT J
+#define D14_BIT 1
+
+#define D15_PORT J
+#define D15_BIT 0
+
+#define D16_PORT H
+#define D16_BIT 1
+
+#define D17_PORT H
+#define D17_BIT 0
+
+#define D18_PORT D
+#define D18_BIT 3
+
+#define D19_PORT D
+#define D19_BIT 2
+
+#define D20_PORT D
+#define D20_BIT 1
+
+#define D21_PORT D
+#define D21_BIT 0
+
+#define D22_PORT A
+#define D22_BIT 0
+
+#define D23_PORT A
+#define D23_BIT 1
+
+#define D24_PORT A
+#define D24_BIT 2
+
+#define D25_PORT A
+#define D25_BIT 3
+
+#define D26_PORT A
+#define D26_BIT 4
+
+#define D27_PORT A
+#define D27_BIT 5
+
+#define D28_PORT A
+#define D28_BIT 6
+
+#define D29_PORT A
+#define D29_BIT 7
+
+#define D30_PORT C
+#define D30_BIT 7
+
+#define D31_PORT C
+#define D31_BIT 6
+
+#define D32_PORT C
+#define D32_BIT 5
+
+#define D33_PORT C
+#define D33_BIT 4
+
+#define D34_PORT C
+#define D34_BIT 3
+
+#define D35_PORT C
+#define D35_BIT 2
+
+#define D36_PORT C
+#define D36_BIT 1
+
+#define D37_PORT C
+#define D37_BIT 0
+
+#define D38_PORT D
+#define D38_BIT 7
+
+#define D39_PORT G
+#define D39_BIT 2
+
+#define D40_PORT G
+#define D40_BIT 1
+
+#define D41_PORT G
+#define D41_BIT 0
+
+#define D42_PORT L
+#define D42_BIT 7
+
+#define D43_PORT L
+#define D43_BIT 6
+
+#define D44_PORT L
+#define D44_BIT 5
+
+#define D45_PORT L
+#define D45_BIT 4
+
+#define D46_PORT L
+#define D46_BIT 3
+
+#define D47_PORT L
+#define D47_BIT 2
+
+#define D48_PORT L
+#define D48_BIT 1
+
+#define D49_PORT L
+#define D49_BIT 0
+
+#define D50_PORT B
+#define D50_BIT 3
+
+#define D51_PORT B
+#define D51_BIT 2
+
+#define D52_PORT B
+#define D52_BIT 1
+
+#define D53_PORT B
+#define D53_BIT 0
+
+#define D54_A0_PORT F
+#define D54_A0_BIT 0
+
+#define D55_A1_PORT F
+#define D55_A1_BIT 1
+
+#define D56_A2_PORT F
+#define D56_A2_BIT 2
+
+#define D57_A3_PORT F
+#define D57_A3_BIT 3
+
+#define D58_A4_PORT F
+#define D58_A4_BIT 4
+
+#define D59_A5_PORT F
+#define D59_A5_BIT 5
+
+#define D60_A6_PORT F
+#define D60_A6_BIT 6
+
+#define D61_A7_PORT F
+#define D61_A7_BIT 7
+
+#define D62_A8_PORT K
+#define D62_A8_BIT 0
+
+#define D63_A9_PORT K
+#define D63_A9_BIT 1
+
+#define D64_A10_PORT K
+#define D64_A10_BIT 2
+
+#define D65_A11_PORT K
+#define D65_A11_BIT 3
+
+#define D66_A12_PORT K
+#define D66_A12_BIT 4
+
+#define D67_A13_PORT K
+#define D67_A13_BIT 5
+
+#define D68_A14_PORT K
+#define D68_A14_BIT 6
+
+#define D69_A15_PORT K
+#define D69_A15_BIT 7
